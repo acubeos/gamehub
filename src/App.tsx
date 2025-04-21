eb6bff58c54906e84306b3e283748c351c93a34e
acubeos <80309463+acubeos@users.noreply.github.com>
2025-04-21 12:17:51 +0100

    set up the template

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import { Button } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 
 function App() {
-	return <Button colorPalette='teal'>Click me</Button>
+	return (
+		<Grid
+			templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+		>
+			<GridItem bg='coral' area={"nav"}>
+				Nav
+			</GridItem>
+			<Show above='lg'>
+				<GridItem bg='green' area={"aside"}>
+					Asides
+				</GridItem>
+			</Show>
+			<GridItem bg='blue' area='main'>
+				Content
+			</GridItem>
+		</Grid>
+	)
 }
 
 export default App

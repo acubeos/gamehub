d8c45e66982e9abba88bc613891a4753e74d4c38
acubeos <80309463+acubeos@users.noreply.github.com>
2025-04-21 13:10:05 +0100

    Setup dark mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image, Text } from "@chakra-ui/react"
 import logo from "../assets/Logo.png"
+import { ColorModeButton } from "./ui/color-mode"
 const NavBar = () => {
 	return (
 		<HStack>
 			<Image src={logo} boxSize='50px' />
 			<Text>Home</Text>
+			<ColorModeButton />
 		</HStack>
 	)
 }
